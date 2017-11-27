87c742a86c75c6ce1cf3d943f71bb2f7ce9bb2a7
vidsav <savnikvid5@gmail.com>
2017-11-27 15:07:46 +0100

    added menu.php and new array

--- includes/arrays.php
+++ includes/arrays.php
@@ -56,6 +56,51 @@
         
         
         
-    );
+                    );
+
+    $menuItems = array(
+        
+                    "club-sandwich" => array(
+                        
+                        title => "Club Sandwich",
+                        price => 11,
+                        blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        drink => "Club Soda"
+                    
+                    
+                    ),
+        
+                    "dill-salmon" => array(
+                        
+                        title => "Lemon &amp; Dill Salmon",
+                        price => 18,
+                        blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        drink => "Fancy wine"
+                    
+                    
+                    ),
+        
+                    "super-salad" => array(
+                        
+                        title => "The Super Salad<sup>&reg;</sup>",
+                        price => 34,
+                        blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        drink => "Jug o' water"
+                    
+                    
+                    ),
+        
+                    "mexicana-barbacoa" => array(
+                        
+                        title => "Mexicana Barbacopa",
+                        price => 23,
+                        blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                        drink => "Beer with a lime"
+                    
+                    
+                    ),
+    
+    
+                );
 
 ?>
